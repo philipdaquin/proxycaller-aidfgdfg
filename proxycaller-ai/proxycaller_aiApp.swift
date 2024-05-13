c7dd21fef5d26d912070a626cf077db2cfa007c5
philipdaquin <philipdaquin20@gmail.com>
2024-05-13 12:32:38 +0800

    Initial Commit

---
+++ proxycaller-ai/proxycaller_aiApp.swift
@@ -0,0 +1,17 @@
+//
+//  proxycaller_aiApp.swift
+//  proxycaller-ai
+//
+//  Created by Philip Daquin on 13/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct proxycaller_aiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
